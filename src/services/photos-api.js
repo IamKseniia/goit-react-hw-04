c51251d2da4da67d1a7d865687ef970075a38ee9
Kseniia Safronova <yakseniya1994@gmail.com>
2025-03-16 17:04:41 +0100

    edited API

--- src/services/photos-api.js
+++ src/services/photos-api.js
@@ -1,18 +1,3 @@
-// import axios from 'axios';
-
-// const ACCESS_KEY = 'aoijLXA1KGzoMKMJ5M3TNHR_79Pjcu6ivDc9hYTB_Es';
-
-// axios.defaults.headers = {
-//   Authorization: `Client-ID ${ACCESS_KEY}`,
-// };
-
-// export const fetchPhotos = async (query, page, perPage) => {
-//   const response = await axios.get(
-//     `https://api.unsplash.com/search/photos?query=${query}&page=${page}&per_page=${perPage}`
-//   );
-//   return response.data;
-// };
-
 import axios from 'axios';
 
 const ACCESS_KEY = 'aoijLXA1KGzoMKMJ5M3TNHR_79Pjcu6ivDc9hYTB_Es';
@@ -26,17 +11,3 @@ export const fetchPhotos = async (query, page, perPage) => {
     `/search/photos?page=${page}&per_page=${perPage}&query=${query}`
   );
 };
-
-// import axios from 'axios';
-
-// const ACCESS_KEY = 'aoijLXA1KGzoMKMJ5M3TNHR_79Pjcu6ivDc9hYTB_Es';
-
-// axios.defaults.baseURL = 'https://api.unsplash.com';
-// axios.defaults.headers.common['Authorization'] = `Client-ID ${ACCESS_KEY}`;
-// axios.defaults.headers.common['Accept-Version'] = 'v1';
-
-// export const fetchPhotos = async (query, page, perPage) => {
-//   return axios.get(
-//     `/search/photos?page=${page}&per_page=${perPage}&query=${query}`
-//   );
-// };
